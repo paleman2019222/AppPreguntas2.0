399650d1150ece70587d1a04d0ecf5d671326aeb
Petter99 <mcanil1999@gmail.com>
2023-04-22 10:14:57 -0600

    cambios menu, borde y degradado

--- app/src/main/java/com/example/apppreguntas/MainActivity.java
+++ app/src/main/java/com/example/apppreguntas/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.apppreguntas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void toast(View view)
+    {
+        Toast.makeText(getApplicationContext(),"hola",Toast.LENGTH_LONG).show();
     }
 
 }
