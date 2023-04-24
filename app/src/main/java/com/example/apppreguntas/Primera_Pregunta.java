27479b38e3e396b4163d95972683143a8a2165da
Petter99 <mcanil1999@gmail.com>
2023-04-23 19:20:28 -0600

    ACTIVITIS Y FONDO

---
+++ app/src/main/java/com/example/apppreguntas/Primera_Pregunta.java
@@ -0,0 +1,14 @@
+package com.example.apppreguntas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Primera_Pregunta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primera_pregunta);
+    }
+}
