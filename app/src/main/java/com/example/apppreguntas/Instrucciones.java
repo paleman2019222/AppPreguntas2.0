56d29bdaa743f7e2dff71627a95653c37d4ab9cf
Samuelcitalan <131422504+Samuelcitalan@users.noreply.github.com>
2023-04-23 19:51:57 -0600

    Activity Instrucciones

---
+++ app/src/main/java/com/example/apppreguntas/Instrucciones.java
@@ -0,0 +1,14 @@
+package com.example.apppreguntas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Instrucciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instrucciones);
+    }
+}
