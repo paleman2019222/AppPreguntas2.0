54bc0d5e2345c8b09fed56c7440c55b49e5de5b6
Samuelcitalan <131422504+Samuelcitalan@users.noreply.github.com>
2023-04-24 21:59:20 -0600

    avance cuarta activity

--- app/src/main/java/com/example/apppreguntas/Cuarta_Pregunta.java
+++ app/src/main/java/com/example/apppreguntas/Cuarta_Pregunta.java
@@ -3,12 +3,29 @@ package com.example.apppreguntas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Cuarta_Pregunta extends AppCompatActivity {
+
+    Button res1, res2, res3, res4;
+
+    int categoria, punteo;
+
+    TextView Pregunta;
+
+    //respuestas correctas
+    String Respuesta_Juego = "luigi";
+    String Respuesta_Mate = "Isaac Newton";//
+    String Respuesta_Nac = "Quetzal";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cuarta_pregunta);
     }
+
+
+
+
 }
