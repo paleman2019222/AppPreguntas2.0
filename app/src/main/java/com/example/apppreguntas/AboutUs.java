ba9dc76423d81d1346a8fb4475427a2002961662
Petter99 <mcanil1999@gmail.com>
2023-04-24 23:06:49 -0600

    diseño y botones regresar

--- app/src/main/java/com/example/apppreguntas/AboutUs.java
+++ app/src/main/java/com/example/apppreguntas/AboutUs.java
@@ -2,7 +2,9 @@ package com.example.apppreguntas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutUs extends AppCompatActivity {
 
@@ -11,5 +13,11 @@ public class AboutUs extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
 
+    }
+    public void regresar(View v)
+    {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
--- app/src/main/java/com/example/apppreguntas/Instrucciones.java
+++ app/src/main/java/com/example/apppreguntas/Instrucciones.java
@@ -2,7 +2,9 @@ package com.example.apppreguntas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Instrucciones extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class Instrucciones extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instrucciones);
+    }
+
+    public void regresar(View v)
+    {
+       Intent i = new Intent(this, MainActivity.class);
+       startActivity(i);
+       finish();
     }
 }
